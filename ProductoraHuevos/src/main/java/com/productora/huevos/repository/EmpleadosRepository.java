0fc6d0ab9ff369f155cbd51daa180668e3e0cc7c
dennisly <dgranadosl@ucenfotec.ac.cr>
2019-07-18 20:20:54 -0600

    Se crean los clases pertinentes para el dominio y el repositorio, de acuerdo al standard de JPA, data spring.

---
+++ ProductoraHuevos/src/main/java/com/productora/huevos/repository/EmpleadosRepository.java
@@ -0,0 +1,8 @@
+package com.productora.huevos.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.productora.huevos.domain.Empleados;
+
+public interface EmpleadosRepository extends JpaRepository<Empleados, Long>{
+
+}
---
+++ ProductoraHuevos/src/main/java/com/productora/huevos/repository/FincasRepository.java
@@ -0,0 +1,8 @@
+package com.productora.huevos.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.productora.huevos.domain.Fincas;
+
+public interface FincasRepository extends JpaRepository<Fincas, Long>{
+
+}
